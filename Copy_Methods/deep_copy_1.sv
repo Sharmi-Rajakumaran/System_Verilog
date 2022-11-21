a3592097810238edd3dc8369ff074b9431c4940c
SHARMI RAJAKUMARAN <106381150+Sharmi-Rajakumaran@users.noreply.github.com>
2022-11-21 14:49:58 +0530

    Update deep_copy_1.sv

--- Copy_Methods/deep_copy_1.sv
+++ Copy_Methods/deep_copy_1.sv
@@ -30,6 +30,7 @@ endclass: sub
            t_h1.data = 4;
            t_h1.sub_h.obj = 5;
            t_h2 = t_h1.copy; // First memory is allocated, then copy method is executed
+           t_h2.sub_h.obj = 10;
            
      endmodule: test
       
@@ -38,3 +39,7 @@ endclass: sub
  /*
     Step1 : create a function copy of class type (ie the function itself will return the class object)
     step2: take class object and put it in the new memory copy.properties
+    step2: in every class include a copy method.
+    // The copy method of top class calls the lower level class copy methods also
+    
+    */
