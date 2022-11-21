2e12cee458b3eb65cbe62eab86398d17c86efe8e
SHARMI RAJAKUMARAN <106381150+Sharmi-Rajakumaran@users.noreply.github.com>
2022-11-21 10:05:27 +0530

    Create threads_eg3.sv

---
+++ threads/threads_eg3.sv
@@ -0,0 +1,19 @@
+module threads();
+  initial
+    begin
+      fork 
+        for(int H1_pointer =0; H1_pointer <2; H1_pointer++)
+          begin
+            #1 $display($time, "FIRST LOOP: Value of H1 = %g", H1_pointer);
+          end
+        
+        for(int D1_channel =2; D1_channel>0; D1_channel--)
+          begin
+            #1 $display($time, "SECOND LOOP: Value of D1 = %g", D1_channel);
+          end
+      join_none
+      
+      $display("@%g Outside of FORK_JOIN \n", $time);
+      #3 $finish;
+    end
+endmodule
